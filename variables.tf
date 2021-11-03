d53cd14a49379f0d8c0ded8c7159da59bf420ee4
Martin Wiser <martin@Martins-MacBook-Pro.local>
2021-11-03 17:48:03 -0600

    added additional variables

--- variables.tf
+++ variables.tf
@@ -13,3 +13,28 @@ variable "enable_telegraf" {
   type        = bool
   default     = false
 }
+
+variable "observation_dataset" {
+  type        = string
+  description = "Name of dataset to derive kubernetes resources from."
+  default     = "Observation"
+}
+
+variable "max_expiry" {
+  type        = number
+  description = "Maximum expiry time for resources in minutes."
+  default     = 90
+}
+
+variable "freshness_overrides" {
+  type        = map(string)
+  description = "Freshness overrides by dataset. If absent, fall back to freshness_default"
+  default     = {}
+}
+
+variable "freshness_default" {
+  type        = string
+  description = "Default dataset freshness. Can be overridden with freshness input"
+  default     = "1m"
+}
+
